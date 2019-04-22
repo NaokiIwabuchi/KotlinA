e72a41cb65014860313dcb90bbd66dd655675fce
naoki.iwabuchi <worldwideromona@gmail.com>
2019-04-22 22:58:19 +0900

    計算式を入れてみました。

--- app/src/main/java/iwabuchi/naoki/techacademy/kotlina/MainActivity.kt
+++ app/src/main/java/iwabuchi/naoki/techacademy/kotlina/MainActivity.kt
@@ -10,9 +10,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var num = 10
-        Log.d("kotlintest", Integer.toString(num))
-        num = 50
-        Log.d("kotlintest", num.toString())
+        var num1 = 10 + 5 - 2 * 4 / 2
+        Log.d("kotlintest", "10+5-2*4/2= " + num1)
+
+        val flag1 = true
+        val flag2 = false
+        Log.d("kotlintest", "flag1 && flag2 = " + (flag1 && flag2 ))
+        Log.d("kotlintest", "flag1 || flag2 = " + (flag1 || flag2 ))
+
+        val num2 = 10
+        val num3 = 30
+        Log.d("kotlintest", "num2 < num3 =" + (num2 < num3))
+
     }
+
+
 }
